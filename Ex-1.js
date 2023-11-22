57fc26790876110d3b953445665e7ae23e1f7f13
Alessio <carninoalessio@gmail.com>
2023-11-22 11:12:49 +0100

    Ex-2 completed

--- Ex-1.js
+++ Ex-1.js
@@ -27,5 +27,3 @@ if ((userYear % 4 === 0 && userYear % 100 !== 0) || userYear % 400 === 0) {
 } else {
   alert(`Hi ${userName}, your year of birth was NOT a leap year`);
 }
-
-console.log(userName, userYear);
