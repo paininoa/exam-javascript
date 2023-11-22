fed155e69b95f5143f859705cd24be567dc6d02d
Alessio <carninoalessio@gmail.com>
2023-11-22 11:51:12 +0100

    Ex-4 completed

--- Ex-4.js
+++ Ex-4.js
@@ -7,13 +7,30 @@ Crea una pagina HTML con un <input> di testo, un elemento <button> , un elemento
 
 Scrivi codice JavaScript che aggiunge un ascoltatore di eventi al <button> che,
 quando cliccato, se l'utente non ha scritto nulla nell’<input> :
-inserisce all'interno del <p> " errorMessage " un messaggio di errore che dice "Per
+- inserisce all'interno del <p> " errorMessage " un messaggio di errore che dice "Per
 favore, inserisci del testo."; (5 punti)
 
 Altrimenti, se c’è del testo all’interno dell’ <input> :
-svuota il contenuto del <p> " errorMessage "; (5 punti)
+- svuota il contenuto del <p> " errorMessage "; (5 punti)
 
-inserisce all'interno del <ul> " tasks " un nuovo <li> che ha come contenuto ciò
+- inserisce all'interno del <ul> " tasks " un nuovo <li> che ha come contenuto ciò
 che è stato inserito dall’utente nell’ <input>; (10 punti)
 
 */
+
+window.addEventListener("load", () => {
+  const input = document.getElementById("inp");
+  const button = document.getElementById("btn");
+  const p = document.getElementById("errorMessage");
+  const ul = document.getElementById("tasks");
+
+  button.addEventListener("click", () => {
+    if (input.value === "") {
+      p.innerHTML = `Per favore, inserisci del testo.`;
+    } else {
+      p.innerHTML = ``;
+
+      ul.innerHTML += `<li>${input.value}</li>`;
+    }
+  });
+});
